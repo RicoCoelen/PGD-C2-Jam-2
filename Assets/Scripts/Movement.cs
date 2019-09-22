f1801903a0dbfc822ab328c282a5c9396bb1754e
ThomasO02 <thomas.otte02@gmail.com>
2019-09-22 17:39:57 +0200

    Added ground and players, players can move using WASD(p1) or arrow keys(p2)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 velocity;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Vector3.zero;
+
+        if (tag == "Player1")
+        {
+
+            if (Input.GetKey("w"))
+            {
+                velocity.y += 1;
+            }
+            if (Input.GetKey("a"))
+            {
+                velocity.x -= 1;
+            }
+            if (Input.GetKey("s"))
+            {
+                velocity.y -= 1;
+            }
+            if (Input.GetKey("d"))
+            {
+                velocity.x += 1;
+            }
+        }
+        if(tag == "Player2")
+        {
+            if (Input.GetKey("up"))
+            {
+                velocity.y += 1;
+            }
+            if (Input.GetKey("left"))
+            {
+                velocity.x -= 1;
+            }
+            if (Input.GetKey("down"))
+            {
+                velocity.y -= 1;
+            }
+            if (Input.GetKey("right"))
+            {
+                velocity.x += 1;
+            }
+        }
+
+
+        velocity *= speed * Time.deltaTime;
+        transform.position += velocity;
+    }
+}
